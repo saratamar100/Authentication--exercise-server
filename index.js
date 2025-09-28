6f3a3f3bd39808a8e609508fd449b874cdb2133a
שרה תמר אמיתי <saratama@g.jct.ac.il>
2025-09-28 14:39:07 +0300

    step 2

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 5000;
 
 const apiRouter = require("./routes/api");
-app.use("/api", apiRouter);
+app.use(cors());
 app.use(express.json());
+app.use("/api", apiRouter);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
