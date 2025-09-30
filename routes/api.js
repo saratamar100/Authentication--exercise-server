91cac7fbbe7a88a4e8b08312cd7e8aa134a88220
שרה תמר אמיתי <saratama@g.jct.ac.il>
2025-09-30 11:34:02 +0300

    register

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { genToken } = require("../utils/utils");
 const { isLoggedIn } = require("../middlewares");
 const { addToken, removeToken } = require("../tokenService");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
 const books = [
@@ -9,7 +10,12 @@ const books = [
   { id: 2, title: "To Kill a Mockingbird", author: "Harper Lee" },
   { id: 3, title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
 ];
-const users = [{ username: "sara", password: "p1" }];
+const users = [
+  {
+    username: "sara",
+    password: "$2a$10$bcq8gLKqq0vOU42QZf4hNOZlMZCJKQ5TswD5U3b340RxMz/cEUAGC",
+  },
+];
 router.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -20,7 +26,7 @@ router.get("/books", isLoggedIn, (req, res) => {
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
   const user = users.find(
-    (u) => u.username === username && u.password === password
+    (u) => u.username === username && bcrypt.compareSync(password, u.password)
   );
   if (user) {
     const token = genToken(user);
@@ -30,7 +36,6 @@ router.post("/login", (req, res) => {
     res.status(401).json({ message: "Invalid credentials" });
   }
 });
-//logout
 router.post("/logout", isLoggedIn, (req, res) => {
   const token = req.headers.authorization.split(" ")[1];
   const isRemoved = removeToken(token);
@@ -38,6 +43,15 @@ router.post("/logout", isLoggedIn, (req, res) => {
     return res.json({ msg: "Logged out" });
   }
   return res.status(400).json({ msg: "Invalid token" });
+});
+router.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  const userExists = users.some((u) => u.username === username);
+  if (userExists) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+  users.push({ username, password: bcrypt.hashSync(password, 10) });
+  res.json({ message: "Registration successful" });
 });
 
 module.exports = router;
